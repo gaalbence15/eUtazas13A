6942adf9b161e7b3058a3e622ccc497a6a153148
gaalbence15 <gaalbence15@gmail.com>
2020-10-06 09:40:00 +0200

    Upgrade

--- src/Megoldas.ts
+++ src/Megoldas.ts
@@ -1,5 +1,13 @@
-import Felszállás from './Felszallas';
+import fs from "fs";
+import Felszállás from "./Felszallas";
 
-export default class Megoldas{
+export default class Megoldas {
     private _utasAdatok: Felszállás[] = [];
+
+    constructor(forras: string) {
+        fs.readFileSync(forras)
+            .toString()
+            .split("\n")
+            .forEach(i => {});
+    }
 }
